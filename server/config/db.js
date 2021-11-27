8caed9d461990ade1dd521184d210ba5bfc433e8
Jayesh Amodkar <jayeshdamodkar@gmail.com>
2021-11-27 18:50:00 -0500

    code change

--- server/config/db.js
+++
@@ -1,6 +0,0 @@
-module.exports = 
-{
-    //"URI": "mongodb://localhost/book_store",
-    "URI": "mongodb+srv://Webengers:webengerspass@tournament.gdc0d.mongodb.net/Tournament?retryWrites=true&w=majority",
-    "Secret": 'SomeSecret'
-}
