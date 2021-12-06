30634b6a792da92d9e9cf674bf88e070f918795a
Reihaneh Namdari <r3ihan3h@yahoo.com>
2021-12-06 16:03:52 -0500

    firebase database changes

--- Tournament/src/app/app.module.ts
+++ Tournament/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//firebase
+import {AngularFireModule} from '@angular/fire';
+import { AngularFireAuthModule} from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import {environment} from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -47,7 +52,8 @@ export function jwtTokenGetter(): string
   ],
   imports: [
     BrowserModule,
-    
+    AngularFireModule.initializeApp(environment.firebase),
+
     AppRoutingModule,
     TournamentModule,
     
--- Tournament/src/environments/environment.ts
+++ Tournament/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyArwkg_3Qhdj76GODRD-CT7mpZnOIaCjQY",
+    authDomain: "tournament-a4480.firebaseapp.com",
+    databaseURL: "https://tournament-a4480-default-rtdb.firebaseio.com",
+    projectId: "tournament-a4480",
+    storageBucket: "tournament-a4480.appspot.com",
+    messagingSenderId: "501650314805",
+    appId: "1:501650314805:web:c48e08a0d1e9aad105037c"
+  }
 };
 
 /*
